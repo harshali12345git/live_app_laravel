5c3e7b4019398f50ec3f8f4538061e5eb956a316
harshali <dreamitdiksha@gmail.com>
2021-11-26 19:43:21 +0530

    tag commmit

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TagResource;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\OfficeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagController;
 
 
-Route::get('/tags',[TagController::class]);
+Route::get('/tags', [TagController::class]);
+
+
+Route::get('/tags', TagController::class);
+Route::get('/offices', [OfficeController::class, 'index']);
