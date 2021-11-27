d9df56990d5868d6a163682e615b08adfc28f2a2
diksha-sangare <dreamitdiksha@gmail.com>
2021-11-27 11:47:34 +0530

    list offices end point

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,10 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Tag;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TagFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Tag::class;
+
     /**
      * Define the model's default state.
      *
@@ -14,7 +22,7 @@ class TagFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word,
         ];
     }
 }
